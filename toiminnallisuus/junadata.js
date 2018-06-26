a99c382f5f12283caf04812b2c292c4fd0f44e77
LoserPanda <losarpanda82@gmail.com>
2018-06-26 16:49:24 +0300

    junien oikeat ajat ja pysäkit

--- toiminnallisuus/junadata.js
+++ toiminnallisuus/junadata.js
@@ -30,48 +30,21 @@ function junienTulokset(tulos) {
         var juna = tulos[i];
 
         for (var j = 0; j < juna.timeTableRows.length; j++) {
-            if (juna.timeTableRows[j].stationShortCode === document.getElementById("lahtoasema").value && juna.timeTableRows[j].type === "DEPARTURE" ||
-                juna.timeTableRows[j].stationShortCode === document.getElementById("paateasema").value && juna.timeTableRows[j].type === "ARRIVAL") {
-                console.dir(juna.timeTableRows[j].stationShortCode);
+            if (juna.timeTableRows[j].stationShortCode === document.getElementById("lahtoasema").value && juna.timeTableRows[j].type === "DEPARTURE") {
+                var lahtopaikka = juna.timeTableRows[j].stationShortCode;
+                var lahtoaika = new Date(juna.timeTableRows[j].scheduledTime).toLocaleTimeString("fi", optiot);
+            }
+            if (juna.timeTableRows[j].stationShortCode === document.getElementById("paateasema").value && juna.timeTableRows[j].type === "ARRIVAL") {
+                var paamaara = juna.timeTableRows[j].stationShortCode;
+                var saapumisaika = new Date(juna.timeTableRows[j].scheduledTime).toLocaleTimeString("fi", optiot);
             }
         }
-    }
 
-    //console.dir(juna);
-}
-
-/*function junienTulokset(tulos) {
-    while (lista.firstChild) {
-        lista.removeChild(lista.firstChild);
-    }
-    var optiot = {hour: '2-digit', minute: '2-digit', hour12: false};
-    for (var i = 0; i < 2; ++i) {
-        var elem = document.createElement("li");
-        var juna = tulos[i];
-
-        for (var j = 0; j < timeTableRows.length; ++j) {
-            //TODO hae junan lähtö- ja pääteasema
-            if (juna.timeTableRows[j].stationShortCode === lahtoasema.value && juna.timeTableRows[j].type === "DEPARTURE") {
-                //console.dir(new Date(juna.timeTableRows[j].scheduledTime).toLocaleTimeString("fi", optiot));
-                console.dir(juna.timeTableRows[j].stationShortCode);
-            }
-
-            if (juna.timeTableRows[j].stationShortCode === paateasema.value && juna.timeTableRows[j].type === "ARRIVAL") {
-                //console.dir(new Date(juna.timeTableRows[j].scheduledTime).toLocaleTimeString("fi", optiot));
-                console.dir(juna.timeTableRows[i].stationShortCode);
-                console.dir("hei");
-            }
-            //console.dir(juna.timeTableRows[j].stationShortCode + j);
-            //console.dir(lahtoasema.value);
-            console.dir(paateasema.value + " hei " + juna.timeTableRows.length);
-        }
-
-        var lahtoaika = new Date(juna.timeTableRows[0].scheduledTime).toLocaleTimeString("fi", optiot);
-        var saapumisaika = new Date(juna.timeTableRows[juna.timeTableRows.length - 1].scheduledTime).toLocaleTimeString("fi", optiot);
-        elem.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + ": " + juna.trainType + juna.trainNumber + ", lähtee: " + lahtoaika + " saapuu: " + saapumisaika));
+        elem.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + " " + juna.trainType + juna.trainNumber +
+            " lähtee paikasta " + lahtopaikka + " klo " + lahtoaika + "  ja saapuu paikkaan " + paamaara + " klo " + saapumisaika));
         lista.appendChild(elem);
     }
-}*/
+}
 
 function kaannaJunanTyyppi(juna) {
     if (juna.trainCategory === "Long-distance") return "Kaukojuna";

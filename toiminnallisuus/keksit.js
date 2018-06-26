5a0ecec962113df48089a57713ccfe3c89785868
nikopit <niko.pitkonen@helsinki.fi>
2018-06-26 15:33:26 +0300

    lisaa muokkausta kekseihin

--- toiminnallisuus/keksit.js
+++ toiminnallisuus/keksit.js
@@ -2,36 +2,27 @@
 //Funktio luo cookiesit käyttäjälle.
 function asetaKeksi(keksinNimi, keksinArvo, voimassaolo) {
     var pvm = new Date();
-    pvm.setTime(pvm.getTime() + (voimassaolo * 24 * 60 * 60 * 1000));
+    pvm.setTime(pvm.getTime() + (voimassaolo * 24 * 60 * 60));
     var voimassaoloaika = "päättyy="+pvm.toUTCString();
     document.cookie = keksinNimi + "=" + keksinArvo + ";" + voimassaoloaika + ";path=/";
 }
 
 //Alin funktio käyttää tätä tietojen tarkistamiseen.
-function getKeksi(keksinNimi) {
-    var name = keksinNimi + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+function getKeksi(name) {
+    var regexp = new RegExp("(?:^" + name + "|;\s*" + name + ")=(.*?)(?:;|$)", "g");
+    var result = regexp.exec(document.cookie);
+    return (result === null) ? null : result[1];
 }
 
 // Funktio käynnistyy kun sivu avataan. Kysyy käyttäjän nimeä, jos keksejä ei vielä ole.
 function tarkistaKeksi() {
     var kayttaja = getKeksi("username");
-    if (kayttaja != "") {
+    if (kayttaja != null) {
         alert("Tervetuloa takaisin, " + kayttaja);
     } else {
         kayttaja = prompt("Taidat olla täällä ensimmäistä kertaa. Anna nimesi:", "");
         if (kayttaja != "" && kayttaja != null) {
-            asetaKeksi("username", kayttaja, 365);
+            asetaKeksi("username", kayttaja, 30);
         }
     }
 }

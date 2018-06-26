746b70d4e679429f98856aa02a2754ae6dd14eae
LoserPanda <losarpanda82@gmail.com>
2018-06-26 10:03:39 +0300

    haku lahtoajan perusteella

---
+++ toiminnallisuus/junadata.js
@@ -0,0 +1,32 @@
+var lista = document.getElementById("tulos");
+var pyynto = new XMLHttpRequest();
+pyynto.onreadystatechange = function () {
+    if (pyynto.readyState === 4 && pyynto.status === 200) {
+        var tulos = JSON.parse(pyynto.responseText);
+        console.dir(tulos);
+        junienTulokset(tulos);
+    }
+};
+
+function junienTulokset(tulos) {
+    var optiot = {hour: '2-digit', minute: '2-digit', hour12: false}; //kohtia Tommin koodista
+    for (var i = 0; i < 10; ++i) {
+        var elem = document.createElement("li");
+        var juna = tulos[i];
+        var lahtoaika = new Date(juna.timeTableRows[0].scheduledTime).toLocaleTimeString("fi", optiot);
+        var saapumisaika = new Date(juna.timeTableRows[juna.timeTableRows.length - 1].scheduledTime).toLocaleTimeString("fi", optiot);
+        elem.appendChild(document.createTextNode(juna.trainCategory + ": " + juna.trainType + juna.trainNumber + ", lähtee: " + lahtoaika + " saapuu: " + saapumisaika));
+        lista.appendChild(elem);
+    }
+}
+
+function haedata() {
+    var alku ="https://rata.digitraffic.fi/api/v1";
+    var keski = "/live-trains/station/";
+    var viimeinen = "/";
+    var lahtoasema = document.getElementById("lahtoasema").value;
+    var paateasema = document.getElementById("paateasema").value;
+    pyynto.open('get', alku + keski + lahtoasema + viimeinen + paateasema);
+    console.log(alku + keski + lahtoasema + viimeinen + paateasema);
+    pyynto.send();
+}
---
+++ toiminnallisuus/lahtoasemat.js
@@ -0,0 +1,32 @@
+var lahtoasema = document.getElementById("lahtoasema");
+var asemahaku = new XMLHttpRequest();
+asemahaku.onreadystatechange = function () {
+    if (asemahaku.readyState === 4 && asemahaku.status === 200) {
+        var tulokset = JSON.parse(asemahaku.responseText);
+        console.dir(tulokset);
+        lajitteleAsemat(tulokset);
+    }
+};
+//ainakin kuopioon klo 16.?? menevä IC puuttuu, koska juna katkeaa kouvolassa ja numero muuttuu
+function lajitteleAsemat(tulokset) {
+    for (var i = 0; i < tulokset.length; ++i) {
+        var asema = tulokset[i];
+        var tunnus = asema.stationShortCode;
+        var kokonimi = asema.stationName;
+        var elementti = document.createElement("option");
+        elementti.setAttribute("value", tunnus);
+        elementti.text=kokonimi;
+        var elementti2 = document.createElement("option");
+        elementti2.setAttribute("value", tunnus);
+        elementti2.text=kokonimi;
+        lahtoasema.appendChild(elementti);
+        paateasema.appendChild(elementti2);
+    }
+}
+
+function haeAsemat() {
+    asemahaku.open('get', "https://rata.digitraffic.fi/api/v1/metadata/stations");
+    console.log("https://rata.digitraffic.fi/api/v1/metadata/stations");
+    asemahaku.send();
+}
+haeAsemat();

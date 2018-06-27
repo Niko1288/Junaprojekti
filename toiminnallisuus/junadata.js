8eabea1f17b59c58b4a70283e5c230268e94f3ab
Annina Heiskanen <anninaheiskanen@gmail.com>
2018-06-27 15:24:32 +0300

    taulukko

--- toiminnallisuus/junadata.js
+++ toiminnallisuus/junadata.js
@@ -18,8 +18,37 @@ function junienTulokset(tulos) {
     poistaEdellinenLista();
     var optiot = {hour: '2-digit', minute: '2-digit', hour12: false};
     var lkmListalle = document.getElementById("tulostenLkm").value;
+
+    var divi = document.createElement("div");
+    var tulosOtsikko = document.createElement("h2");
+    tulosOtsikko.appendChild(document.createTextNode("Junat välillä " + document.getElementById("lahtoasema").value + " - " + document.getElementById("paateasema").value));
+    divi.appendChild(tulosOtsikko);
+    var pvm = document.createElement("p");
+    pvm.appendChild(document.createTextNode("27.6.2018")); //muuta automaattiseksi
+    divi.appendChild(pvm);
+    lista.appendChild(divi);
+
+    var table = document.createElement("table");
+    var tBody = document.createElement("tbody");
+    var tHead = document.createElement("thead");
+    var otsikkoRivi = document.createElement("tr");
+    var otsikko1 = document.createElement("th");
+    otsikko1.appendChild(document.createTextNode("Juna"));
+    var otsikko2 = document.createElement("th");
+    otsikko2.appendChild(document.createTextNode("Lähtöaika"));
+    var otsikko3 = document.createElement("th");
+    otsikko3.appendChild(document.createTextNode("Saapumisaika"));
+    var otsikko4 = document.createElement("th");
+    otsikko4.appendChild(document.createTextNode("Matkan kesto"));
+    otsikkoRivi.appendChild(otsikko1);
+    otsikkoRivi.appendChild(otsikko2);
+    otsikkoRivi.appendChild(otsikko3);
+    otsikkoRivi.appendChild(otsikko4);
+    tHead.appendChild(otsikkoRivi);
+    table.appendChild(tHead);
+    table.appendChild(tBody);
+
     for (var i = 0; i < lkmListalle; i++) {
-        var elem = document.createElement("li");
         var juna = tulos[i];
 
         try {
@@ -41,13 +70,43 @@ function junienTulokset(tulos) {
 
             if (juna.trainCategory === document.getElementById("tyyppi").value || document.getElementById("tyyppi").value === "kaikki") {
                 if (juna.trainCategory === "Commuter") {
-                    elem.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + " " + juna.commuterLineID +
-                        " lähtee paikasta " + lahtopaikka + " klo " + lahtoaika + "  ja saapuu paikkaan " + paamaara + " klo " + saapumisaika + ". Matkan kesto " + matkanKesto(lahtoaikaKesto, saapumisaikaKesto) + "."));
-                    lista.appendChild(elem);
+                    var tulosRivi = document.createElement("tr");
+                    var tieto1 = document.createElement("td");
+                    tieto1.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + " " + juna.commuterLineID));
+                    var tieto2 = document.createElement("td");
+                    tieto2.appendChild(document.createTextNode(lahtoaika));
+                    var tieto3 = document.createElement("td");
+                    tieto3.appendChild(document.createTextNode(saapumisaika));
+                    var tieto4 = document.createElement("td");
+                    tieto4.appendChild(document.createTextNode(matkanKesto(lahtoaikaKesto, saapumisaikaKesto)));
+                    tulosRivi.appendChild(tieto1);
+                    tulosRivi.appendChild(tieto2);
+                    tulosRivi.appendChild(tieto3);
+                    tulosRivi.appendChild(tieto4);
+                    tBody.appendChild(tulosRivi);
+                    lista.appendChild(table);
+                    // elem.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + " " + juna.commuterLineID +
+                    //     " lähtee paikasta " + lahtopaikka + " klo " + lahtoaika + "  ja saapuu paikkaan " + paamaara + " klo " + saapumisaika + ". Matkan kesto " + matkanKesto(lahtoaikaKesto, saapumisaikaKesto) + "."));
+                    // lista.appendChild(elem);
                 } else {
-                    elem.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + " " + juna.trainType + juna.trainNumber +
-                        " lähtee paikasta " + lahtopaikka + " klo " + lahtoaika + "  ja saapuu paikkaan " + paamaara + " klo " + saapumisaika + ". Matkan kesto " + matkanKesto(lahtoaikaKesto, saapumisaikaKesto) + "."));
-                    lista.appendChild(elem);
+                    var tulosRivi2 = document.createElement("tr");
+                    var tieto5 = document.createElement("td");
+                    tieto5.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + " " + juna.trainType + juna.trainNumber));
+                    var tieto6 = document.createElement("td");
+                    tieto6.appendChild(document.createTextNode(lahtoaika));
+                    var tieto7 = document.createElement("td");
+                    tieto7.appendChild(document.createTextNode(saapumisaika));
+                    var tieto8 = document.createElement("td");
+                    tieto8.appendChild(document.createTextNode(matkanKesto(lahtoaikaKesto, saapumisaikaKesto)));
+                    tulosRivi2.appendChild(tieto5);
+                    tulosRivi2.appendChild(tieto6);
+                    tulosRivi2.appendChild(tieto7);
+                    tulosRivi2.appendChild(tieto8);
+                    tBody.appendChild(tulosRivi2);
+                    lista.appendChild(table);
+                //     elem.appendChild(document.createTextNode(kaannaJunanTyyppi(juna) + " " + juna.trainType + juna.trainNumber +
+                //         " lähtee paikasta " + lahtopaikka + " klo " + lahtoaika + "  ja saapuu paikkaan " + paamaara + " klo " + saapumisaika + ". Matkan kesto " + matkanKesto(lahtoaikaKesto, saapumisaikaKesto) + "."));
+                //     lista.appendChild(elem);
                 }
             } else {
                 lkmListalle++;
